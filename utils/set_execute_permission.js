65ca3f463063922f13edc13206cfa0d07c8ec323
Mátyás Budavári <budavariam@gmail.com>
2020-12-15 21:16:08 +0100

    Use path.join to concatenate folder path

--- utils/set_execute_permission.js
+++ utils/set_execute_permission.js
@@ -1,4 +1,5 @@
 "use strict";
 const fs = require("fs");
+const path = require("path")
 
-fs.chmodSync('./dist/revealjs-cli.js', 0o755)
+fs.chmodSync(path.join('.', 'dist', 'revealjs-cli.js'), 0o755)
